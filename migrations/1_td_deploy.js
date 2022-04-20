7dea2b1b2ba7fa4d4b86fcaf5986f1be4a1c36fa
Marc-Antoine Ourradour <83420479+Marco75116@users.noreply.github.com>
2022-04-21 00:43:20 +0200

    Add files via upload

--- migrations/1_td_deploy.js
+++ migrations/1_td_deploy.js
@@ -1,63 +1,84 @@
-const Str = require('@supercharge/strings')
+const Str = require("@supercharge/strings");
 // const BigNumber = require('bignumber.js');
 
 var TDErc20 = artifacts.require("ERC20TD.sol");
 var evaluator = artifacts.require("Evaluator.sol");
 var evaluator2 = artifacts.require("Evaluator2.sol");
-
+var exsolutions = artifacts.require("ExerciceSolution.sol");
 
 module.exports = (deployer, network, accounts) => {
-    deployer.then(async () => {
-        await deployTDToken(deployer, network, accounts); 
-        await deployEvaluator(deployer, network, accounts); 
-        await setPermissionsAndRandomValues(deployer, network, accounts); 
-        await deployRecap(deployer, network, accounts); 
-    });
+  deployer.then(async () => {
+    await deployTDToken(deployer, network, accounts);
+    await deployEvaluator(deployer, network, accounts);
+    await doTD(deployer, network, accounts);
+  });
 };
 
 async function deployTDToken(deployer, network, accounts) {
-	TDToken = await TDErc20.new("TD-ERC721-101","TD-ERC721-101",web3.utils.toBN("0"))
-	
-	// TDToken = await TDErc20.at("0x46a9Dc47185F769ef9a11927B0f9d2fd0dEc3304")
+  TDToken = await TDErc20.at("0x8B7441Cb0449c71B09B96199cCE660635dE49A1D");
 }
 
 async function deployEvaluator(deployer, network, accounts) {
-	Evaluator = await evaluator.new(TDToken.address)
-	// Evaluator = await evaluator.at("0x6B19d275dA33857a3f35F7c1034048Ba1abF75CD") 
-	Evaluator2 = await evaluator2.new(TDToken.address)
+  Evaluator = await evaluator.at("0xa0b9f62A0dC5cCc21cfB71BA70070C3E1C66510E");
+  Evaluator2 = await evaluator2.at(
+    "0x4f82f7A130821F61931C7675A40fab723b70d1B8"
+  );
 }
-
-async function setPermissionsAndRandomValues(deployer, network, accounts) {
-	await TDToken.setTeacher(Evaluator.address, true)
-	await TDToken.setTeacher(Evaluator2.address, true)
-	randomNames = []
-	randomLegs = []
-	randomSex = []
-	randomWings = []
-	for (i = 0; i < 20; i++)
-		{
-		randomNames.push(Str.random(15))
-		randomLegs.push(Math.floor(Math.random()*5))
-		randomSex.push(Math.floor(Math.random()*2))
-		randomWings.push(Math.floor(Math.random()*2))
-		// randomTickers.push(web3.utils.utf8ToBytes(Str.random(5)))
-		// randomTickers.push(Str.random(5))
-		}
-
-	console.log(randomNames)
-	console.log(randomLegs)
-	console.log(randomSex)
-	console.log(randomWings)
-	// console.log(web3.utils)
-	// console.log(type(Str.random(5)0)
-	await Evaluator.setRandomValuesStore(randomNames, randomLegs, randomSex, randomWings);
-	await Evaluator2.setRandomValuesStore(randomNames, randomLegs, randomSex, randomWings);
+account = "0x88E95624f03251970501Da6e3fA6f93F93B06890";
+async function deployerSolution(deployer, network, accounts) {
+  ExerciceSolution = await exsolutions.new("MYNFT", "MFT", Evaluator.address, {
+    from: account,
+  });
+  console.log("ExerciceSolution " + ExerciceSolution.address);
 }
-
-async function deployRecap(deployer, network, accounts) {
-	console.log("TDToken " + TDToken.address)
-	console.log("Evaluator " + Evaluator.address)
-	console.log("Evaluator2 " + Evaluator2.address)
+async function getBalance(account, network, accounts) {
+  let balance = await TDToken.balanceOf(account);
+  console.log("Balance " + balance);
 }
+async function doTD(deployer, network, accounts) {
+  console.log("start td");
+  await getBalance(account, network, accounts);
+  await deployerSolution(deployer, network, accounts, { from: account });
 
+  // console.log("------Exercice1------");
+  await Evaluator.submitExercice(ExerciceSolution.address, { from: account });
+  // await Evaluator.ex1_testERC721({ from: account });
+  // await getBalance(account, network, accounts);
 
+  // console.log("------Exercice2------");
+  // await Evaluator.ex2a_getAnimalToCreateAttributes({ from: account });
+  // _name = await Evaluator.readName(account);
+  // _wings = await Evaluator.readWings(account);
+  // _legs = await Evaluator.readLegs(account);
+  // _sex = await Evaluator.readSex(account);
+  // await ExerciceSolution.declareAnimal(_sex, _legs, _wings, _name, {
+  //   from: account,
+  // });
+
+  // console.log(await ExerciceSolution.ownerOf(2));
+  // await Evaluator.ex2b_testDeclaredAnimal(2, { from: account });
+  // await getBalance(account, network, accounts);
+
+  // console.log("------Exercice3------");
+  // await Evaluator.ex3_testRegisterBreeder({ from: account });
+  // await getBalance(account, network, accounts);
+
+  // console.log("------Exercice4------");
+  // await Evaluator.ex4_testDeclareAnimal({ from: account });
+  // await getBalance(account, network, accounts);
+
+  // console.log("------Exercice5------");
+  // await ExerciceSolution.mint(account, { from: account });
+  // await Evaluator.ex5_declareDeadAnimal({ from: account });
+  // await getBalance(account, network, accounts);
+
+  // console.log("------Exercice6a------");
+  // await Evaluator.ex6a_auctionAnimal_offer({ from: account });
+  // await getBalance(account, network, accounts);
+
+  console.log("------Exercice6b------");
+  await ExerciceSolution.mint(account, { from: account });
+  await ExerciceSolution.offerForSale(2, "100", { from: account });
+  await Evaluator.ex6b_auctionAnimal_buy(2, { from: account });
+  await getBalance(account, network, accounts);
+}
